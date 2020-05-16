274be43388f887e5e2acdc3d23da704864b5e74d
Mitali J <mitalijuneja1@mitalis-mbp-2.lan>
2020-05-16 14:37:17 -0700

    finished personal website

--- Personal Website/app.py
+++ Personal Website/app.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 14:57:17 2020
 
-@author: etill
-"""
 
 #import statements
 from flask import Flask, render_template
@@ -13,9 +8,33 @@ app = Flask(__name__)
 
 #static route
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template("index.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
 
 #start the server
 if __name__ == "__main__":
     app.run()
+    
+    
+    
+"""
+   #b1ded6 (light blue)
+   #99d1ab (light green)
+   #87c4b6 (dark blue)
+   #e8a692 (orange)
+   #eba7b0 (light pink)
+   #de7896 (dark pink) 
+
+"""
+    
+   
